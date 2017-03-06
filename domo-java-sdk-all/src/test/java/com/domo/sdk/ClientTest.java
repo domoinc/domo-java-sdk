d61a27d2b0a9ff6f65996d96c30e6650405da353
Clint Checketts <clint.checketts@domo.com>
2017-03-06 12:50:34 -0700

    Initial commit

--- domo-java-sdk-all/src/test/java/com/domo/sdk/ClientTest.java
+++ domo-java-sdk-all/src/test/java/com/domo/sdk/ClientTest.java
@@ -15,8 +15,6 @@ public class ClientTest {
                 "2cdcc6c2a4d94dd444ff3d648ff0254d8f1d442a1c43d38c1f96e405fb4691fd");
 
 
-        client.userClient().create(false, )
-
         assertThat(client.userClient().list(1, 0)).hasSize(1);
     }
 
